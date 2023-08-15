39898608f02b69f1b444be30eceec084945d707e
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-08-15 01:04:08 -0300

    Professionals.tsx created

---
+++ src/pages/Professionals.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "../components/Header";
+
+function Professionals() {
+    return (
+        <div id="container">
+            <div id='header-container'>
+                <Header />
+            </div>
+        </div>
+    )
+}
+
+export default Professionals
