56afd3e21fe78a61d0d50ff0921dc71d1ab76e8c
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-08-15 20:11:48 -0300

    Professionals.tsx updated (optimization)

--- src/pages/Professionals.tsx
+++ src/pages/Professionals.tsx
@@ -4,10 +4,8 @@ import Header from "../components/Header";
 
 function Professionals() {
     return (
-        <div id="container">
-            <div id='header-container'>
-                <Header />
-            </div>
+        <div id='container'>
+            <Header />
         </div>
     )
 }
