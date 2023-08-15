55aebd355272c9904ba791dcbe1d8f727a3e6a89
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-08-15 20:11:25 -0300

    LandingPage.tsx updated

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,15 +3,10 @@ import Header from "../components/Header";
 
 import '../styles/pages/landingPage.css'
 
-function LandingPage(){
-    return(
+function LandingPage() {
+    return (
         <div id='container'>
-            <div id='header-container'>
-                <Header />
-            </div>
-            <div id='body-container'>
-                    <span>BEM-VINDO A GOODJOB</span>
-                </div>
+            <Header />
         </div>
     );
 }
