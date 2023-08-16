862744308e0fdce57917d9302d4cb4c0eff573e6
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-08-16 12:12:01 -0300

    Scheduling.tsx created

--- src/pages/Scheduling.tsx
+++ src/pages/Scheduling.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 
+import Header from "../components/Header";
+
 function Scheduling() {
     return(
-        <div></div>
+        <div id='container'>
+            <Header />
+            <div id="scheduling-container">
+
+            </div>
+        </div>
     )
 }
 
