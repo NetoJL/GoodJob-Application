80476796f97b7efe26c7df8cb2f6af9e8ce19804
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-08-17 00:06:50 -0300

    services.ts created and updated

---
+++ src/types/services.ts
@@ -0,0 +1,9 @@
+export interface ServiceProps{
+    id: string;
+    name: string;
+    description: string;
+    duration: string;
+    price: string;
+    professional: string;
+    availability: string[];
+}
