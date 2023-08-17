906fe34d3e251402b0c3c21753c834537e65f681
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-08-17 00:06:03 -0300

    ServiceList.tsx updated

--- src/components/ServiceList.tsx
+++ src/components/ServiceList.tsx
@@ -2,14 +2,24 @@ import React from "react";
 
 import '../styles/components/serviceList.css'
 
-function ServiceList(props: any) {
+import { ServiceProps } from '../types/services'
+
+interface ServicesProps {
+    service: ServiceProps;
+}
+
+/*const ServicesList: React.FC<ServicesProps> = ({ service }) => {*/
+function ServicesList(props:any) {
     return (
         <div onClick={props.goTo} id="service-list">
-            <span>SERVIÇO: {props.service}</span>
+            <span>SERVIÇO: {props.name}</span>
             <span>DESCRIÇÃO: {props.description}</span>
+            <span>DURAÇÃO: {props.length}</span>
+            <span>PREÇO: {props.price}</span>
+            <span>PROFISSIONAL: {props.professional}</span>
             <span>HORÁRIOS: {props.availability}</span>
         </div>
     )
 }
 
-export default ServiceList
+export default ServicesList
